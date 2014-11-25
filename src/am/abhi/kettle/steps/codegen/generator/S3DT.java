42dc5a7109c49d44a19ebc2908850373df6f19df
Abhishek Dey Das <abhishek@abhis.ws>
2014-11-24 20:54:37 -0600

    Added S3 support for dependency management. Added progress update on GUI. Bug fixes. Updated README.

---
+++ src/am/abhi/kettle/steps/codegen/generator/S3DT.java
@@ -0,0 +1,34 @@
+package am.abhi.kettle.steps.codegen.generator;
+
+public class S3DT {
+	private String accessKey;
+	private String secretKey;
+	private String libext;
+	private String libswt;
+
+
+	public String getAccessKey() {
+		return accessKey;
+	}
+	public void setAccessKey(String accessKey) {
+		this.accessKey = accessKey;
+	}
+	public String getSecretKey() {
+		return secretKey;
+	}
+	public void setSecretKey(String secretKey) {
+		this.secretKey = secretKey;
+	}
+	public String getLibext() {
+		return libext;
+	}
+	public void setLibext(String libext) {
+		this.libext = libext;
+	}
+	public String getLibswt() {
+		return libswt;
+	}
+	public void setLibswt(String libswt) {
+		this.libswt = libswt;
+	}
+}
